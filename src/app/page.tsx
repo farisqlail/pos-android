aa6bd3518f90e518f6abdcad8babcfac766732c1
farisqlail <faris.riskilail@gmail.com>
2024-11-21 19:50:58 +0700

    add: zoom in zoom out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,41 @@ const IndexPage = () => {
     router.push("/auth");
   }, [router]);
 
+  useEffect(() => {
+    // Mencegah zoom dengan event listener
+    const preventZoom = (e: TouchEvent) => {
+      if (e.touches.length > 1) {
+        e.preventDefault();
+      }
+    };
+
+    // Menangani zoom melalui keyboard atau gesture
+    const preventKeyboardZoom = (e: KeyboardEvent) => {
+      if (
+        (e.ctrlKey && (e.key === "+" || e.key === "-")) || // Ctrl + Zoom
+        e.key === "Meta" // Command Key
+      ) {
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener("touchmove", preventZoom, { passive: false });
+    document.addEventListener("keydown", preventKeyboardZoom);
+
+    return () => {
+      document.removeEventListener("touchmove", preventZoom);
+      document.removeEventListener("keydown", preventKeyboardZoom);
+    };
+  }, []);
+
   return (
     <div>
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1"
+        />
       </Head>
     </div>
   );
